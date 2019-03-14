dc708404d1751077fdf68b4e51e765cfbcdd0b25
sotheara-leang <leangsotheara@gmail.com>
2019-03-14 11:26:22 +0100

    Commit pytorch

--- main/conf/configuration.py
+++ main/conf/configuration.py
@@ -12,7 +12,7 @@ from singleton_decorator import singleton
 class Configuration:
 
     def __init__(self):
-        logging.info('init config.yml')
+        logging.debug('init config.yml')
 
         with open(os.path.join(os.path.dirname(__file__), "config.yml"), 'r') as file:
             self.cfg = yaml.load(file, Loader=Loader)
